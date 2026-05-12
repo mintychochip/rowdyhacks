61339750dc7843045541dc73b22c5ef97abfc2f0
mintychochip <jlo2@csub.edu>
2026-05-12 07:45:14 -0700

    fix(migrations): correct down_revision for site_config table

--- backend/alembic/versions/2026_05_11_add_site_config.py
+++ backend/alembic/versions/2026_05_11_add_site_config.py
@@ -1,7 +1,7 @@
 """Add site_config table.
 
 Revision ID: 2026_05_11_add_site_config
-Revises: 64fca8aeb3cd
+Revises: 3e2ab12a7512
 Create Date: 2026-05-11 00:00:00.000000
 
 """
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "2026_05_11_add_site_config"
-down_revision: Union[str, None] = "64fca8aeb3cd"
+down_revision: Union[str, None] = "3e2ab12a7512"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
